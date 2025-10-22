0f2236ff4f084c44a9e7d46ccbda5df40ea6a90a
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-10-22 19:38:52 +0100

    D

--- destina_backend/settings.py
+++ destina_backend/settings.py
@@ -98,6 +98,14 @@ DATABASES = {
     )
 }
 
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
