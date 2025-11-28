6f9a224520f5001872b82dabd5108780887115af
emmanuel <emmanuel@example.com>
2025-11-28 20:05:48 +0000

    fg2

--- users/urls.py
+++ users/urls.py
@@ -48,4 +48,6 @@ urlpatterns = [
     path('disable-user/', DisableUserView.as_view(), name='disable-user'),
     path('enable-user/', EnableUserView.as_view(), name='enable-user'),
     path('delete-user/', DeleteUserView.as_view(), name='delete-user'),
+    path('update-user-location/', UpdateUserLocationView.as_view(), name='update-user-location'),
+    path('all-user-locations/', GetAllUserLocationsView.as_view(), name='all-user-locations'),
 ]
